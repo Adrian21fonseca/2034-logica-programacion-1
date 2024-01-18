18cc539fc6a8631302874ecd368b1e4463a2f380
Adrian Fonseca <adrian2020fm@gmail.com>
2024-01-18 13:05:51 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,8 @@
+let numeroSecreto = 6;
+let numeroUsuario = prompt("intruduzca un numero del 1 al 10 por favor");
+
+console.log(numeroUsuario)
+
+if (numeroUsuario == numeroSecreto){
+    alert ('BIEN!!! Has acertado el numero')
+}

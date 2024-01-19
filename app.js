61b3d61b1f705fe4b7fcadc056802fb440a484bb
Adrian Fonseca <adrian2020fm@gmail.com>
2024-01-19 18:05:05 -0500

    first commit

--- app.js
+++ app.js
@@ -1,10 +1,23 @@
 let numeroSecreto = 6;
-let numeroUsuario = prompt("intruduzca un numero del 1 al 10 por favor");
+let numeroUsuario = 0;
+let numeroIntento = 1;
+let palabraVez = "vez";
+
+while(numeroUsuario != numeroSecreto){
+    numeroUsuario = prompt("intruduzca un numero del 1 al 10 por favor");
 
 console.log(numeroUsuario)
 
-if (numeroUsuario == numeroSecreto){
-    alert (`BIEN!!! Has acertado,  el numero es: ${numeroUsuario}`)
-} else{
-    alert('Lo siento no has adivinado el numero')
-}; 
+    if (numeroUsuario == numeroSecreto){
+        alert (`BIEN!!! Has acertado,  el numero es: ${numeroUsuario}. Lo hicistes en ${numeroIntento} ${palabraVez}`)
+    } else{
+        if(numeroUsuario >numeroSecreto){
+            alert("el numero secreto es menor")
+        }else{
+            alert("el numero secreto es mayor")
+        }
+        numeroIntento = numeroIntento + 1;
+        palabraVez = "veces";
+       //alert('Lo siento no has adivinado el numero')
+    };
+};

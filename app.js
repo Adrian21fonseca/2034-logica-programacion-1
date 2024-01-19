6b90b0bc99d8a0e93175460385d11b9ff8af1a81
Adrian Fonseca <adrian2020fm@gmail.com>
2024-01-18 21:19:32 -0500

    first commit

--- app.js
+++ app.js
@@ -4,5 +4,7 @@ let numeroUsuario = prompt("intruduzca un numero del 1 al 10 por favor");
 console.log(numeroUsuario)
 
 if (numeroUsuario == numeroSecreto){
-    alert ('BIEN!!! Has acertado el numero')
-}
+    alert (`BIEN!!! Has acertado,  el numero es: ${numeroUsuario}`)
+} else{
+    alert('Lo siento no has adivinado el numero')
+}; 

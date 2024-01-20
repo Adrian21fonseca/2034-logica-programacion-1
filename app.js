ae7392629fdab012bff690f8634a1f704c92d663
Adrian Fonseca <adrian2020fm@gmail.com>
2024-01-20 12:14:55 -0500

    first commit

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
-let numeroSecreto = 6;
+let numeroSecreto = Math.floor(Math.random()*10)+1;
 let numeroUsuario = 0;
 let numeroIntento = 1;
 let palabraVez = "vez";
+let intentosMaximos = 4;
+
+console.log(numeroSecreto);
 
 while(numeroUsuario != numeroSecreto){
     numeroUsuario = prompt("intruduzca un numero del 1 al 10 por favor");
@@ -16,8 +19,13 @@ console.log(numeroUsuario)
         }else{
             alert("el numero secreto es mayor")
         }
-        numeroIntento = numeroIntento + 1;
+        numeroIntento++;
         palabraVez = "veces";
+
+        if(numeroIntento > intentosMaximos){
+            alert(`has llegado al numero maximo de ${intentosMaximos} intentos`)
+            break;
+        }
        //alert('Lo siento no has adivinado el numero')
     };
 };
